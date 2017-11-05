2b20e8050f9307a68c99a4186d87d173719d1164
alailispring <libdqd@126.com>
2017-11-05 20:29:34 +0800

    standard programme!

--- src/singleton/Singleton1.java
+++ src/singleton/Singleton1.java
@@ -5,12 +5,15 @@ package singleton;
  * Created by AlailiSpring on 2016/11/24.
  */
 public class Singleton1 {
-    private Singleton1(){}
-    private static Singleton1 singleton1=null;
 
-    public static Singleton1 getSingleton1(){
-        if(singleton1==null){
-            singleton1=new Singleton1();
+    private Singleton1() {
+    }
+
+    private static Singleton1 singleton1 = null;
+
+    public static Singleton1 getSingleton1() {
+        if (singleton1 == null) {
+            singleton1 = new Singleton1();
         }
         return singleton1;
     }
